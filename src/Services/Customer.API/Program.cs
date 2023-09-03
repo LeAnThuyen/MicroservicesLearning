9174a9467f8a03f33aedd8d73445ef93a0ea1239
Tana <88257054+LeAnThuyen@users.noreply.github.com>
2023-09-03 12:39:26 +0700

    Create Minimal API for Customer API

--- src/Services/Customer.API/Program.cs
+++ src/Services/Customer.API/Program.cs
@@ -1,5 +1,11 @@
 using Common.Logging;
+using Contracts.Common.Interfaces;
 using Customer.API.Persistence;
+using Customer.API.Repositories;
+using Customer.API.Repositories.Interfaces;
+using Customer.API.Services;
+using Customer.API.Services.Interfaces;
+using Infrastructure.Common;
 using Microsoft.EntityFrameworkCore;
 using Serilog;
 
@@ -16,10 +22,21 @@ try
     // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
     builder.Services.AddEndpointsApiExplorer();
     builder.Services.AddSwaggerGen();
+
+    builder.Services.AddScoped<ICustomerRepository, CustomerRepository>()
+         .AddScoped(typeof(IUnitOfWork<>), typeof(UnitOfWork<>))
+                .AddScoped(typeof(IRepositoryBaseAsync<,,>), typeof(RepositoryBaseAsync<,,>))
+                .AddScoped<ICustomerService, CustomerService>();
+
     var connectionString = builder.Configuration.GetConnectionString("DefaultConnectionString");
     builder.Services.AddDbContext<CustomerContext>(options => options.UseNpgsql(connectionString));
     var app = builder.Build();
-
+    app.Map("/", () => "Welcome to Customer API !");
+    app.MapGet("/api/customers", async (ICustomerService customerService) => await customerService.GetCustomersAsync());
+    app.MapGet("/api/customers/{username}", async (string username, ICustomerService customerService) => await customerService.GetCustomerByUserNameAsync(username));
+    app.MapPost("/", () => "Welcome to Customer API !");
+    app.MapPut("/", () => "Welcome to Customer API !");
+    app.MapDelete("/", () => "Welcome to Customer API !");
     // Configure the HTTP request pipeline.
     if (app.Environment.IsDevelopment())
     {
