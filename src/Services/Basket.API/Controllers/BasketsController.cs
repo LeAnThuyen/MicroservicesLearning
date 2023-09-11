1f19588cf539431d0c308544f9af11f26ba51f4a
Tana <88257054+LeAnThuyen@users.noreply.github.com>
2023-09-12 00:14:29 +0700

    Create Basket API

---
+++ src/Services/Basket.API/Controllers/BasketsController.cs
@@ -0,0 +1,48 @@
+﻿using Basket.API.Entities;
+using Basket.API.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+
+namespace Basket.API.Controllers
+{
+
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class BasketsController : ControllerBase
+    {
+
+        private readonly IBasketRepository _repository;
+        public BasketsController(IBasketRepository repository)
+        {
+            _repository = repository;
+        }
+
+
+        [HttpGet("{userName}")]
+        [ProducesResponseType(typeof(Cart), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult> GetBasketByUserName([Required] string userName)
+        {
+            var result = await this._repository.GetBasketByUsername(userName);
+            return Ok(result ?? new Cart());
+        }
+
+        [HttpPost(Name = "UpdateBasket")]
+        public async Task<IActionResult> UpdateBasket([FromBody] Cart cart)
+        {
+            var option = new DistributedCacheEntryOptions().SetAbsoluteExpiration(DateTime.UtcNow.AddDays(1)).SetSlidingExpiration(TimeSpan.FromMinutes(5));
+
+            var result = await _repository.UpdateBasket(cart, option);
+            return Ok(result);
+        }
+
+        [HttpDelete("{userName}", Name = "DeleteBasket")]
+        [ProducesResponseType(typeof(bool), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<bool>> DeleteBaseket([Required] string userName)
+        {
+            var result = await _repository.DeleteBasketFromUsername(userName);
+            return result;
+        }
+    }
+}
---
+++ src/Services/Basket.API/Controllers/HomComtroller.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Basket.API.Controllers
+{
+    public class HomComtroller : ControllerBase
+    {
+
+        public IActionResult Index()
+        {
+            return Redirect("~/swagger");
+        }
+    }
+}
